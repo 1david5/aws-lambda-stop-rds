98e2812b104c66112d438167a6ef566572c34888
David Lozano <david140813@gmail.com>
2020-05-01 14:41:53 -0400

    changed funtion to work on schedule instead of by event

--- stop-rds.py
+++ stop-rds.py
@@ -2,37 +2,40 @@ import botocore
 import boto3
 from botocore.exceptions import ClientError
 
-def lambda_handler(event, context):
+def lambda_handler():
   rds_client = boto3.client('rds')
 
   # Variable definitions
 
-  instance_identifier = event['detail']['SourceIdentifier']
-  instance_arn = event['detail']['SourceArn']
-  instance_state = event['detail']['Message'].split(' ')[-1]
-  print ('The instace %s is in %s state' %(instance_identifier,instance_state))
+  instances = rds_client.describe_db_instances()
 
-  # Getting RDS tags
-  try:
-    state_tag = rds_client.list_tags_for_resource(
-      ResourceName = instance_arn,
-    )
-  except ClientError as e:
-    print('Problem retreving tags: %s' %e)
-    return
+  for instance in instances['DBInstances']:
+    instance_arn = instance['DBInstanceArn']
+    instance_identifier = instance['DBInstanceIdentifier']
+    # print(instance_arn)
+    # print(instance_identifier)
+    # Getting RDS tags
 
-  for tag in state_tag['TagList']:
-    if tag['Key'] == 'Decired-state' and tag['Value'] ==   'stopped':
-      print("Decired-state tag found")
-      try:
-        print('Stopping %s RDS instance' %instance_identifier)
-        stopping = rds_client.stop_db_instance(
-              DBInstanceIdentifier=instance_identifier
-          )
-        print(stopping)
-        print('Instance stopped successfully')
-        return
-      except ClientError as e:
-        print('Stopping instance failed: %s' %e)
-        return
-  return
+    try:
+      instance_tags = rds_client.list_tags_for_resource(ResourceName = instance_arn)
+    except ClientError as e:
+      print('Problem retreving tags for %instance_identifier: %s' %instance_identifier %e)
+      return
+    # print(instance_tags['TagList'])
+
+    for tag in instance_tags['TagList']:
+      if tag['Key'] == 'Decired-state' and tag['Value'] == 'stopped':
+        print('Decired-state tag found with stopped value for instance %s' %instance_identifier)
+        try:
+          print('Stopping %s RDS instance' %instance_identifier)
+          # stopping = rds_client.stop_db_instance(DBInstanceIdentifier = instance_identifier)
+          # print(stopping)
+          print('Instance stopped successfully')
+          break
+        except ClientError as e:
+          print('Stopping %s instance failed: %s' %instance_identifier %e)
+          return
+      else:
+        print('None Decired-state tag found with stopped value for %s instance' %instance_identifier)
+
+lambda_handler()
